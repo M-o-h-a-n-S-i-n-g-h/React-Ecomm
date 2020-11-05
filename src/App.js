16a1e0de334f472e61935d180f7b91dc5d43d87e
Mohansingh Omprakash <53869946+M-o-h-a-n-S-i-n-g-h@users.noreply.github.com>
2020-11-05 14:23:13 +0530

    Added sass and modified

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import HomePage from "./pages/Homepage/Homepage.component";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Mohan Singh Zero to mastery
-        </a>
-      </header>
+      <HomePage />
     </div>
   );
-}
+};
 
 export default App;
