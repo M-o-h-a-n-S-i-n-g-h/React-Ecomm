0e126b0eac4a31f37a6996c1181ef48fb4b040f2
Mohansingh Omprakash <53869946+M-o-h-a-n-S-i-n-g-h@users.noreply.github.com>
2020-11-05 14:22:26 +0530

    Added pages folder for static pages

---
+++ src/pages/Homepage/Homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Homepage.styles.scss";
+import Directory from "../../components/Directory/Directory.component";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default HomePage;
