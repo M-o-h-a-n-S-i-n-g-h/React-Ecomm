a405cfabe80daf92286aeed7f92e6f711e21591f
Mohansingh Omprakash <53869946+M-o-h-a-n-S-i-n-g-h@users.noreply.github.com>
2020-11-06 13:25:37 +0530

    Added Browser Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/Homepage/Homepage.component";
+
+const Hats = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 const App = () => {
   return (
     <div className="App">
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/hats" component={Hats} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 };
